7c408f48c204e3659d9b28a543d765ad933d12e6
Sagar Bisht <sgr26bisht@gmail.com>
2024-03-14 09:38:00 +0530

    using prettier for indentation

--- Week-3/authentication.js
+++ Week-3/authentication.js
@@ -22,23 +22,25 @@ const ALL_USERS = [
     },
 ];
 
-app.use(express.json())
+app.use(express.json());
 
 function userExists(username, password) {
-    return ALL_USERS.find(user => user.username === username && user.password === password);
+    return ALL_USERS.find(
+        (user) => user.username === username && user.password === password
+    );
 }
 
 app.post("/signin", function (req, res) {
-    if(!req.body) return res.sendStatus(400).end();
-    const {username,password} = req.body;
+    if (!req.body) return res.sendStatus(400).end();
+    const { username, password } = req.body;
     if (!userExists(username, password)) {
         return res.status(403).json({
             msg: "User doesnt exist in our in memory db",
         });
     }
     const payload = {
-        username : username
-    }
+        username: username,
+    };
     const token = jwt.sign(payload, jwtPassword);
     return res.json({
         token,
@@ -48,10 +50,10 @@ app.post("/signin", function (req, res) {
 app.get("/users", function (req, res) {
     const token = req.headers.authorization;
     try {
-        const decoded = jwt.verify(token, jwtPassword); 
+        const decoded = jwt.verify(token, jwtPassword);
         const username = decoded.username;
-        const usersArr = ALL_USERS.filter(user => user.username !== username);
-        return res.status(200).json({"users" : usersArr});
+        const usersArr = ALL_USERS.filter((user) => user.username !== username);
+        return res.status(200).json({ users: usersArr });
     } catch (err) {
         return res.status(403).json({
             msg: "Invalid token",
@@ -59,4 +61,4 @@ app.get("/users", function (req, res) {
     }
 });
 
-app.listen(3000)
+app.listen(3000);
