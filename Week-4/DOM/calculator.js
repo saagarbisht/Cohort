4084756dc9a840e7613466be8b71fb9c3d36177b
saagarbisht <sgrcoc3@gmail.com>
2024-03-21 22:42:37 +0530

    replacing async/await with thenables

--- Week-4/DOM/calculator.js
+++ Week-4/DOM/calculator.js
@@ -4,7 +4,8 @@ const app  = express();
 app.get("/", (req,res) => {
     const num1 = req.query.num1
     const num2 = req.query.num2
-    return res.status(200).json({"result" : Number(num1) + Number(num2)});
+    const result = Number(num1) + Number(num2)
+    return res.send(result.toString());
 })
 
 app.listen(3000);
