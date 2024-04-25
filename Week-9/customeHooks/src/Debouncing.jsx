9bc0a712a7d1ea0f1f36855070ccfea257542840
saagarbisht <sgrcoc3@gmail.com>
2024-04-25 23:38:00 +0530

    debouncing in react

--- Week-9/customeHooks/src/Debouncing.jsx
+++ Week-9/customeHooks/src/Debouncing.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
+import useDebounce from './hooks/useDebounce';
+import { useEffect } from 'react';
 
 export default function Debouncing() {
-    
+    const [input,setInput] = useState("");
+    const debounce = useDebounce(input,500);
+    useEffect(() => {
+    },[])
     return (
-        <div>Debouncing</div>
+        <>
+            <div>
+            Debounce Value {debounce}
+            </div>
+            <input 
+            type='text'
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            placeholder='Search'
+            />
+        </>
     )
 }
