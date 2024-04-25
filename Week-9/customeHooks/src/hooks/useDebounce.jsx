a80221afd455f814e8bacb0ab3b09dec519863bc
saagarbisht <sgrcoc3@gmail.com>
2024-04-25 23:37:43 +0530

    debouncing in react

--- Week-9/customeHooks/src/hooks/useDebounce.jsx
+++ Week-9/customeHooks/src/hooks/useDebounce.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
-export default function useDebounce() {
-    return {}
+export default function useDebounce(input,time) {
+    const [value,setValue] = useState("")
+    useEffect(() => {
+        const start = setTimeout(() => {
+            setValue(input)
+        },time)
+        return () => {
+            clearTimeout(start)
+        }
+    },[input])
+    return value
 }
